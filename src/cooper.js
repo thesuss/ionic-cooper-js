6e146a5c362b923998abdb956bfc7895c639bdf3
thesuss <lullisuss@gmail.com>
2016-09-19 16:51:41 +0200

    added age layer to if-statement

--- src/cooper.js
+++ src/cooper.js
@@ -7,27 +7,30 @@ function Person(attr) {
 
 Person.prototype.cooperResult = function(distance) {
   if (this.gender.toLowerCase() == 'male') {
-    this.message = getCooperTestMale(distance);
+    this.message = this.getCooperTestMale(distance);
   } else {
     this.message = 'Result' //this.getCooperTestFemale(distance);
   }
 };
 
-getCooperTestMale = function(distance) {
+Person.prototype.getCooperTestMale = function(distance) {
   //just testing for result for male distance
-  if(distance >= 2700){
-    return 'Excellent';
-  }
-  if(distance >= 2400 && distance <= 2699){
-    return 'Above Average';
-  }
-  if(distance >= 2200 && distance <=2399){
-    return 'Average';
-  }
-  if(distance >= 2100 && distance <=2199){
-    return 'Below Average';
-  }
-  if(distance < 2100){
-    return 'Poor';
+  //now adding age variable
+  if(this.age <=14){
+    if(distance >= 2700){
+      return 'Excellent';
+    }
+    if(distance >= 2400 && distance <= 2699){
+      return 'Above Average';
+    }
+    if(distance >= 2200 && distance <=2399){
+      return 'Average';
+    }
+    if(distance >= 2100 && distance <=2199){
+      return 'Below Average';
+    }
+    if(distance < 2100){
+      return 'Poor';
+    }
   }
 };
