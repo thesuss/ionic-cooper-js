bda225b47e80ff63945281fb1aed1b0b8d5f0ce6
thesuss <lullisuss@gmail.com>
2016-09-19 17:53:03 +0200

    nested ifs are working, now just push on

--- src/cooper.js
+++ src/cooper.js
@@ -16,41 +16,38 @@ Person.prototype.cooperResult = function(distance) {
 Person.prototype.getCooperTestMale = function(distance) {
   //just testing for result for male distance
   //now adding age variable
-
-
-  if(this.age <=14){
+  if(this.age>= 1 && this.age<= 14) {
     if(distance >= 2700){
-      return 'Excellent';
+      return 'Excellent'
     }
     if(distance >= 2400 && distance <= 2699){
-      return 'Above Average';
+      return 'Above Average'
     }
-    if(distance >= 2200 && distance <= 2399){
-      return 'Average';
+    if(distance >= 2200 && distance <=2399){
+      return 'Average'
     }
-    if(distance >= 2100 && distance <= 2199){
-      return 'Below Average';
+    if(distance >= 2100 && distance <=2199){
+      return 'Below Average'
     }
     if(distance < 2100){
-      return 'Poor';
+      return 'Poor'
     }
   }
-  if(this.age == 15 && this.age == 16){
-    debugger;
+  if(this.age>= 15 && this.age<= 16) {
     if(distance >= 2800){
-      return 'Excellent';
+      return 'Excellent'
     }
-    if(distance >= 2450 && distance <= 2799){
-      return 'Above Average';
+    if(distance >= 2500 && distance <= 2799){
+      return 'Above Average'
     }
-    if(distance >= 2300 && distance <= 2499){
-      return 'Average';
+    if(distance >= 2300 && distance <=2499){
+      return 'Average'
     }
-    if(distance >= 2200 && distance <= 2299){
-      return 'Below Average';
+    if(distance >= 2200 && distance <=2299){
+      return 'Below Average'
     }
     if(distance < 2200){
-      return 'Poor';
+      return 'Poor'
     }
   }
 };
