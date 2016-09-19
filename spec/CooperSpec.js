12e3eaf67f2917f424e07b842bce782e5d71c1b5
thesuss <lullisuss@gmail.com>
2016-09-19 15:14:49 +0200

    added distance-attribute to person

--- spec/CooperSpec.js
+++ spec/CooperSpec.js
@@ -1,20 +1,31 @@
 describe('Person', function() {
 
   beforeEach(function() {
-    person = new Person({
-      gender: 'Male',
-      age: 46
-    });
+    girl = new Person({gender: 'female', age: 10, distance: 1000});
+    boy = new Person({gender: 'male', age: 22, distance: 2900});
   });
 
-  it("should have gender", function() {
-    expect(person.gender).toBeDefined();
-    expect(person.gender).toBe('Male');
+  it("should be girl", function() {
+    expect(girl.gender).toEqual('female');
   });
 
-  it("should have age", function() {
-    expect(person.age).toBeDefined();
-    expect(person.age).toBe(46);
+  it("should be 10 years old", function() {
+    expect(girl.age).toEqual(10);
   });
 
+  it("ran 1000 meters", function() {
+    expect(girl.distance).toEqual(1000);
+  });
+
+  it("should be boy", function() {
+    expect(boy.gender).toEqual('male');
+  });
+
+  it("should be 50 years old", function() {
+    expect(boy.age).toEqual(22);
+  });
+
+  it("ran 2900 meters", function() {
+    expect(boy.distance).toEqual(2900);
+  });
 });
